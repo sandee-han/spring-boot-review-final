d5d1c6317e2be00990d5000a1b0b890894d3de43
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-31 11:20:37 +0900

    first commit

---
+++ src/test/java/com/example/springbootreviewfinal/SpringBootReviewFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreviewfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReviewFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
